f60f7d2b8fa11be7c441079113fc86248e31afaf
Thomas Moreau <toma.moral@gmail.com>
2015-02-17 19:21:02 +0100

    Improve Logger
    Add functions to handle logging actions
    Add progress function to log progress
    Add a level setter

--- utils/logger/logger.py
+++ utils/logger/logger.py
@@ -3,24 +3,89 @@ from sys import stdout as out
 import logging
 
 
-class Logger(object):
+class Logger(logging.Logger):
     """Logging handler"""
-    def __init__(self, levl=logging.INFO, **kwargs):
-        super(Logger, self).__init__()
-
-        root = logging.getLogger()
-        root.setLevel(levl)
-        if len(root.handlers) < 1:
+    def __init__(self, levl=logging.INFO, name='root', **kwargs):
+        super(Logger, self).__init__(name, **kwargs)
+        self.setLevel(levl)
+        if len(self.handlers) < 1:
             ch = logging.StreamHandler(out)
             ch.setLevel(levl)
             formatter = logging.Formatter('\r%(levelname)s '
                                           '- %(message)s')
             ch.setFormatter(formatter)
-            root.addHandler(ch)
+            self.addHandler(ch)
+            self.last_wr = ''
+            self.is_prog = False
 
-    def progress(self, i, max_iter):
-        out.write('\b'*6 + '{:6.2%}'.format(i/max_iter))
-        out.flush
+    def progress(self, name, i, max_iter, levl=logging.INFO):
+        if self.level > levl:
+            return
+        if self.last_wr != name:
+            if self.is_prog:
+                out.write('\n')
+            self.is_prog = True
+            out.write('{} - {} - '.format(logging.getLevelName(levl), name))
+            out.write(' '*7)
+        out.write('\b'*7 + '{:7.2%}'.format(i/max_iter))
+        if i == max_iter-1:
+            out.write('\r{} - {} - Done   '
+                      ''.format(logging.getLevelName(levl), name))
+            out.write('\n')
+            self.is_prog = False
+        out.flush()
+        self.last_wr = name
+
+    def set_mode(self, levl=logging.INFO):
+        self.debug('Set mode: {}'.format(logging.getLevelName(levl)))
+        self.setLevel(levl)
+        for ch in self.handlers:
+            ch.setLevel(levl)
+
+    def debug(self, msg, **kwargs):
+        if self.level > 10:
+            return
+        if self.is_prog:
+            out.write('\n')
+        self.is_prog = False
+        self.last_wr = ''
+        super(Logger, self).debug(msg, **kwargs)
+
+    def info(self, msg, **kwargs):
+        if self.level > 20:
+            return
+        if self.is_prog:
+            out.write('\n')
+        self.is_prog = False
+        self.last_wr = ''
+        super(Logger, self).info(msg, **kwargs)
+
+    def Warning(self, msg, **kwargs):
+        if self.level > 30:
+            return
+        if self.is_prog:
+            out.write('\n')
+        self.is_prog = False
+        self.last_wr = ''
+        super(Logger, self).warning(msg, **kwargs)
+
+    def error(self, msg, **kwargs):
+        if self.level > 40:
+            return
+        if self.is_prog:
+            out.write('\n')
+        self.is_prog = False
+        self.last_wr = ''
+        super(Logger, self).error(msg, **kwargs)
+
+    def critical(self, msg, **kwargs):
+        if self.level > 50:
+            return
+        if self.is_prog:
+            out.write('\n')
+        self.is_prog = False
+        self.last_wr = ''
+        super(Logger, self).exception(msg, **kwargs)
 
 
 if __name__ == '__main__':
@@ -29,4 +94,4 @@ if __name__ == '__main__':
     max_iter = 1000000
 
     for i in range(max_iter):
-        log.progress(i, max_iter)
+        log.progress('Counter', i, max_iter)
