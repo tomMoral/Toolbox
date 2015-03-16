0ef5e9f7a6ebf5bb4bceef3d78c4c53fd6bdb6e2
Thomas Moreau <toma.moral@gmail.com>
2015-03-16 13:40:35 +0100

    Improve error handle + thread safe

--- utils/logger/handler_p.py
+++ utils/logger/handler_p.py
@@ -1,15 +1,12 @@
 from sys import stdout as out
 import numpy as np
 import logging
-from multiprocessing import Process, Pipe, Manager, Lock
+from multiprocessing import Process, Pipe, Manager, Lock, Queue
 from collections import defaultdict
 from time import time
-from collections import defaultdict
 
 
 from . import STOP, PROGRESS, SAVE, COST, MODE
-
-manager = Manager()
 
 
 class Handler(Process):
@@ -27,7 +24,7 @@ class Handler(Process):
         SAVE: Object to save/optional nameFile
     """
     def __init__(self, levl=logging.INFO, name='root', graph_update=0.005,
-                 **kwargs):
+                 default_line_style='-o', **kwargs):
         super(Handler, self).__init__(name='Log_process')
         # Get root logger
         self.log = logging.getLogger(name)
@@ -41,16 +38,21 @@ class Handler(Process):
         self.last_writter = ''
         self.unfinished = False
         self.pin, self.pout = Pipe()
-        self.level = manager.Value('i', 0)
+        self.qin = Queue()
+        self._treat = self.qin.get
+        self.manager = Manager()
+        self.level = self.manager.Value('i', 0)
         self.lock = Lock()
         self.set_mode(levl)
         self.graph = defaultdict(lambda: defaultdict(lambda: None))
         self.lst_time = defaultdict(lambda: 0)
+        self.default_line_style = default_line_style
 
     def get_pin(self):
         '''Return the logging pipe
         '''
-        return self.pin
+        return self.qin
+        #return self.pin
 
     def set_mode(self, levl=logging.INFO):
         '''Change the logging level of this handler
@@ -66,7 +68,7 @@ class Handler(Process):
         '''Handler loop'''
         try:
             while True:
-                action, levl, entry = self.pout.recv()
+                action, levl, entry = self._treat()
                 if action == STOP:
                     self._log(logging.DEBUG, 'LOGGER - End the logger')
                     return 0
@@ -83,15 +85,16 @@ class Handler(Process):
                 else:
                     self._log(levl, entry)
         except KeyboardInterrupt:
-            if 10 >= self.level:
-                self._log(10, 'LOGGER - KeyboardInterrupt')
+            if 20 >= self.level:
+                self._log(20, 'LOGGER - KeyboardInterrupt')
         except:
-            import sys
-            e, v = sys.exc_info()[:2]
-            print('Msg', v)
-            print("Test: %s - %s" % e.__name__, v)
+            if 50 >= self.level:
+                import sys
+                e, v = sys.exc_info()[:2]
+                self._log(40, '{} - {}'.format(e.__class__.__name__, v))
         finally:
-            print('HANDELER - quit')
+            if 40 >= self.level:
+                self._log(40, 'HANDELER - quit')
             return 0
 
     def _beggin_line(self):
@@ -132,7 +135,8 @@ class Handler(Process):
         self.last_writter = name
 
     def _graph_cost(self, levl=logging.INFO, cost=0, iteration=1,
-                    name='Cost', curve='cost', end=False):
+                    name='Cost', curve='cost', end=False, linestyle=None,
+                    **kwargs):
         import matplotlib as mpl
         mpl.interactive(True)
         import matplotlib.pyplot as plt
@@ -140,16 +144,21 @@ class Handler(Process):
             print('end')
             self._update_fig(name)
             return
-
         # Update the graph
         line = self.graph[name][curve]
         if line is None:
             plt.figure(name)
-            line = plt.loglog([iteration+1], [cost], '-o', label=curve)[0]
+            line = plt.loglog([iteration], [cost], self.default_line_style,
+                              label=curve, **kwargs)[0]
             plt.legend()
         else:
             line.set_xdata(np.r_[line.get_xdata(), iteration])
             line.set_ydata(np.r_[line.get_ydata(), cost])
+
+        if linestyle is not None:
+            line.set_linestyle(linestyle)
+            line.set_marker('')
+
         self.graph[name][curve] = line
 
         if time()-self.lst_time[name] >= 0.1:
--- utils/logger/logger_p.py
+++ utils/logger/logger_p.py
@@ -19,19 +19,34 @@ class Logger(object):
     references = 0
     _alive = True
 
-    def __init__(self, levl=logging.INFO):
+    def restart():
+        if not Logger.console.is_alive():
+            print('new console')
+            Logger.console = Handler(levl=20)
+            Logger.console.start()
+            Logger._alive = True
+        return Logger.console.get_pin()
+
+    def __init__(self, name='', levl=logging.INFO):
         '''Create a basic Logger and add a console handler
         '''
         super(Logger, self).__init__()
-        self.qin = Logger.console.get_pin()
         self.level = levl
-        self.set_mode(levl)
         Logger.references += 1
+        self.qin = Logger.restart()
+        self.set_mode(levl)
+
+    def _log(self, entry):
+        try:
+            self.qin.put(entry, False)
+        except TimeoutError:
+            print('Fail to log', entry)
 
     def set_mode(self, levl=logging.INFO):
         '''Change the logging level of the logger and the console handler
         '''
-        self.qin.send((MODE, self.level, levl))
+        #self.qin.send((MODE, self.level, levl))
+        self._log((MODE, self.level, levl))
         self.level = levl
 
     def end(self):
@@ -39,7 +54,7 @@ class Logger(object):
         '''
         Logger.references -= 1
         if Logger.references == 0 and Logger._alive:
-            self.qin.send((STOP, None, None))
+            self._log((STOP, None, None))
             Logger.console.join()
             Logger._alive = False
 
@@ -48,23 +63,23 @@ class Logger(object):
 
     def debug(self, msg, **kwargs):
         if self.level <= 10:
-            self.qin.send((LOG, 10,  msg))
+            self._log((LOG, 10,  msg))
 
     def info(self, msg, **kwargs):
         if self.level <= 20:
-            self.qin.send((LOG, 20, msg))
+            self._log((LOG, 20, msg))
 
     def warning(self, msg, **kwargs):
         if self.level <= 30:
-            self.qin.send((LOG, 30, msg))
+            self._log((LOG, 30, msg))
 
     def error(self, msg, **kwargs):
         if self.level <= 40:
-            self.qin.send((LOG, 40, msg))
+            self._log((LOG, 40, msg))
 
     def critical(self, msg, **kwargs):
         if self.level <= 50:
-            self.qin.send((LOG, 50, msg))
+            self._log((LOG, 50, msg))
 
     def progress(self, iteration=0, max_iter=100, name='Progress',
                  levl=logging.INFO, **kwargs):
@@ -80,7 +95,7 @@ class Logger(object):
         if self.level <= levl:
             kwargs.update(dict(iteration=iteration, max_iter=max_iter,
                                name=name))
-            self.qin.send((PROGRESS, levl, kwargs))
+            self._log((PROGRESS, levl, kwargs))
 
     def graphical_cost(self, cost=0, iteration=1, name='Cost',
                        levl=logging.INFO, end=False, **kwargs):
@@ -96,11 +111,11 @@ class Logger(object):
         if self.level <= levl:
             kwargs.update(dict(cost=cost, iteration=iteration,
                                name=name, end=end))
-            self.qin.send((COST, levl, kwargs))
+            self._log((COST, levl, kwargs))
 
     def save(self, levl=logging.INFO, **kwargs):
         if self.level <= levl:
-            self.qin.send((SAVE, levl, kwargs))
+            self._log((SAVE, levl, kwargs))
 
 
 if __name__ == '__main__':
