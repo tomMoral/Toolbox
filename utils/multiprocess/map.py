1bd76672e0abb32dfa54feeeab1f7af77623f2e4
Thomas Moreau <toma.moral@gmail.com>
2015-03-16 13:42:13 +0100

    Genereic Multiprocess map

---
+++ utils/multiprocess/map.py
@@ -0,0 +1,91 @@
+import numpy as np
+import sys
+from utils.logger import Logger
+import multiprocessing
+from multiprocessing import Process, Queue
+log = Logger(name='map')
+
+
+class WorkerGroups(Process):
+    def __init__(self, qin, qout, fun, id_w=0, **kwargs):
+        super(WorkerGroups, self).__init__(name='Worker n°{}'.format(id_w))
+        self.qin = qin
+        self.qout = qout
+        self.id = id_w
+        self.fun = fun
+        self.args = kwargs
+
+    def run(self):
+        idp, p = self.qin.get()
+        while idp is not None:
+            log.debug('Worker {} - |qin| = {}'
+                      ''.format(self.id, self.qin.qsize()))
+            params = dict(p=p, **self.args)
+            self.qout.put((idp, self.fun(**params)))
+            idp, p = self.qin.get()
+        log.debug('Worker {} finished'.format(self.id))
+        return 0
+
+
+def map_grouping(fun, l1, njobs=0, **kwargs):
+
+    if njobs < 1:
+        cc = multiprocessing.cpu_count()
+        njobs = min(cc-1, cc-njobs)
+
+    # Init loop variables
+    N = len(l1)
+    qin = Queue()
+    qout = Queue()
+    resultat = []
+
+    for k, p in enumerate(l1):
+        qin.put((k, p))
+
+    try:
+        slaves = []
+        for i in range(njobs):
+            qin.put((None, None))
+            slaves += [WorkerGroups(qin, qout, fun, id_w=i, **kwargs)]
+            slaves[-1].start()
+
+        idp, r = qout.get(True, 5)
+        resultat = np.empty(N, dtype=type(r))
+        resultat[idp] = r
+        for i in range(N-1):
+            idp, r = qout.get(True, 5)
+            resultat[idp] = r
+            log.progress(max_iter=N, iteration=i,
+                         name='Scoring gouping')
+        for s in slaves:
+            s.join()
+    except:
+        e, v = sys.exc_info()[:2]
+        log.error('Map - {} - {}'.format(e.__class__.__name__, v))
+    finally:
+        for s in slaves:
+            if s.is_alive():
+                s.terminate()
+        return resultat
+
+
+from time import time, sleep
+
+
+def fun(p):
+    sleep(0.001)
+    return p*2
+
+if __name__ == '__main__':
+    try:
+        test = np.arange(1000)
+        t1 = time()
+        res = map_grouping(fun, test)
+        print('Multi core maping: {:.2}s'.format(time()-t1))
+        t1 = time()
+        res2 = list(map(fun, test))
+        print('Std maping: {:.2}s'.format(time()-t1))
+
+        assert((res == res2).all())
+    finally:
+        log.end()
