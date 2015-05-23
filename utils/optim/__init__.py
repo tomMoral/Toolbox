0e77073d73b68ebe6750961c29dbb6d3b84f55a4
Thomas Moreau <toma.moral@gmail.com>
2015-05-23 17:25:42 +0200

    Better base class handle

--- utils/optim/__init__.py
+++ utils/optim/__init__.py
@@ -1,127 +1,32 @@
-from math import sqrt
-import numpy as np
-from utils.logger import Logger
-log = Logger('_GD')
 
 
-class _GradientDescent(object):
-    """Class to hold gradient descent properties"""
-    def __init__(self, problem, decreasing_rate='sqrt',
-                 stop='up5', tol=1e-10, graph_cost=None,
-                 name=None, debug=0):
-        '''Gradient Descent handeler
+def get_log_rate(lr):
+    if lr[:3] == 'log':
+        inc = float(lr[3:]) if len(lr[3:]) > 0 else 2
+        log_rate = (lambda s: lambda t: s*t + (t == 0))(inc)
+    elif lr[:3] == 'lin':
+        inc = int(lr[3:]) if len(lr[3:]) > 0 else 10
+        log_rate = (lambda s: lambda t: t+s)(inc)
+    elif lr == 'none':
+        log_rate = lambda t: 1e300
+    elif type(lr) is int or type(lr) is float:
+        inc = log_rate
+        log_rate = (lambda s: lambda t: s*t + (t == 0))(inc)
+    else:
+        assert False, "{} is not a log rate".format(lr)
 
-        Parameters
-        ----------
-        param: list of the Parameters
-        alpha: learning rate controler
-        grad: function computing the gradient
-            given the current parameters
-        decreasing_rate: {'sqrt', 'linear'} deacreasing rate
-            for the learning rate
-        '''
-        if debug > 0:
-            log.set_level(10)
-        self.pb = problem
-        self.alpha = 4/problem.L
-        self.decreasing_rate = decreasing_rate
-        self.t = 0
-        self.pt = np.copy(problem.x0)
-        self.cost = [self.pb.cost(self.pt)]
-        self.stop = stop
-        self.finished = False
-        self.tol = tol
-        self.log_x = [np.copy(self.pt)]
-        self.graph_cost = graph_cost
-        self.name = name if name is not None else self.__repr__()
+    return log_rate
+
+
+class ImplementationError(Exception):
+    """Implementation Error"""
+    def __init__(self, msg, cls):
+        super(ImplementationError, self).__init__()
+        self.msg = msg
+        self.cls = cls
 
     def __repr__(self):
-        return '_GradientDescent'
-
-    def update(self):
-        '''Update the parameters given
-
-        Parameters
-        ----------
-        grad: list, optional (default: None)
-            list of the gradient for each parameters
-        '''
-        if self.finished:
-            return True
-        self.t += 1
-        self.p_update()
-        cc = self.pb.cost(self.pt)
-        self.cost += [cc]
-        self.log_x += [np.copy(self.pt)]
-        if self.graph_cost is not None:
-            log.graphical_cost(name=self.graph_cost, cost=cc,
-                               curve=self.name)
-        stop = self._stop()
-        if stop:
-            self.end()
-        return stop
-
-    def p_update(self):
-        '''Update the parameters
-        '''
-        grad = self.pb.grad(self.pt)
-        self.p_grad = grad
-        lr = self._get_lr()
-        self.pt = self.pt-lr*grad
-
-    def _get_lr(self):
-        '''Auxillary funciton, return the learning rate
-        '''
-        lr = self.alpha
-        if self.decreasing_rate == 'sqrt':
-            lr /= sqrt(self.t)
-        elif self.decreasing_rate == 'linear':
-            lr /= self.t
-        elif self.decreasing_rate == 'k2':
-            lr *= 2/(self.t+2)
-        elif hasattr(self.decreasing_rate, '__call__'):
-            lr *= self.decreasing_rate(self.t)
-        return lr
-
-    def _stop(self):
-        '''Implement stopping criterion
-        '''
-        # No stopping criterions
-        if self.stop == 'none':
-            return False
-
-        cost = self.cost
-        lc = cost[-1]
-        # If the cost move less than tol, stop
-        if len(cost) > 5 and lc <= cost[-4] < lc+self.tol:
-            log.info('Stop - No advance cost - {}'.format(self.__repr__()))
-            self.finished = True
-            return self.finished
-
-        # If |x_n-x_n-1| < tol, stop
-        if np.sum(np.abs(self.log_x[-1]-self.log_x[-2])) < self.tol:
-            self.finished = True
-            log.info('Stop - No advance X - {}'.format(self.__repr__()))
-            return self.finished
-
-        # Other stopping criterion
-        if self.stop == 'up5':
-            self.finished = (len(cost) > 5 and cost[-5] < lc)
-            if self.finished:
-                log.info('Stop - Up 5 - {}'.format(self.__repr__()))
-            return self.finished
-        else:
-            return False
-
-    def end(self):
-        if self.graph_cost is not None:
-            log.graphical_cost(name=self.graph_cost, curve=self.name,
-                               end=True)
-            self.graph_cost = None
+        return self.cls+'-'+self.msg
 
 
-class GD_Exception(Exception):
-    """Gradient Descent exception"""
-    def __init__(self, msg):
-        super(GD_Exception, self).__init__()
-        self.msg = msg
+from ._gradient_descent import _GradientDescent
---
+++ utils/optim/_gradient_descent.py
@@ -0,0 +1,156 @@
+import numpy as np
+from math import sqrt
+from time import time
+
+from utils.logger import Logger
+log = Logger('_GD', 10)
+
+from . import get_log_rate
+
+
+class _GradientDescent(object):
+    """Class to hold gradient descent properties"""
+
+    id_gd = 0
+
+    def __init__(self, problem, decreasing_rate='sqrt',
+                 stop='', tol=1e-10, graphical_cost=None,
+                 name=None, debug=0, logging=False,
+                 log_rate='log', i_max=1000, t_max=40):
+        '''Gradient Descent handeler
+
+        Parameters
+        ----------
+        param: list of the Parameters
+        alpha: learning rate controler
+        grad: function computing the gradient
+            given the current parameters
+        decreasing_rate: {'sqrt', 'linear'} deacreasing rate
+            for the learning rate
+        '''
+
+        self.id = _GradientDescent.id_gd
+        _GradientDescent.id_gd += 1
+
+        if debug > 0:
+            log.set_level(10)
+            log.debug('Set debug mode on')
+        self.pb = problem
+        self.alpha = 1/problem.L
+        self.decreasing_rate = decreasing_rate
+        self.stop = stop
+        self.finished = False
+        self.tol = tol
+        self.iteration = 0
+        self.t = 0
+
+        # Logging system
+        self.logging = logging
+        if self.logging:
+            self.log_rate = get_log_rate(log_rate)
+        else:
+            self.log_rate = get_log_rate('none')
+        self.next_log = self.log_rate(self.iteration)
+        self.i_max = i_max
+        self.t_max = t_max
+
+        self.name = name if name is not None else '_GD' + str(self.id)
+        self.graph_cost = None
+        if graphical_cost is not None:
+            self.graph_cost = dict(name=graphical_cost, curve=self.name)
+
+    def __repr__(self):
+        return self.name
+
+    def _init_algo(self):
+        pass
+
+    def update(self):
+        '''Update the parameters given
+
+        Parameters
+        ----------
+        grad: list, optional (default: None)
+            list of the gradient for each parameters
+        '''
+        if self.finished:
+            return True
+        if self.iteration == 0:
+            self.start()
+        self.iteration += 1
+        dz = self.p_update()
+        self.t = time() - self.t_start
+        if self.iteration >= self.next_log:
+            log.log_obj(name='cost'+str(self.id), obj=np.copy(self.pb.pt),
+                        iteration=self.iteration+1, fun=self.pb.cost,
+                        graph_cost=self.graph_cost, time=self.t)
+            self.next_log = self.log_rate(self.iteration)
+        stop = self._stop(dz)
+        if stop:
+            self.end()
+        return stop
+
+    def p_update(self):
+        '''Update the parameters
+        '''
+        grad = self.pb.grad(self.pb.pt)
+        self.p_grad = grad
+        lr = self._get_lr()
+        self.pb -= lr*grad
+        return lr*np.sum(grad)
+
+    def _get_lr(self):
+        '''Auxillary funciton, return the learning rate
+        '''
+        lr = self.alpha
+        if self.decreasing_rate == 'sqrt':
+            lr /= sqrt(self.iteration)
+        elif self.decreasing_rate == 'linear':
+            lr /= self.iteration
+        elif self.decreasing_rate == 'k2':
+            lr *= 2/(self.iteration+2)
+        elif hasattr(self.decreasing_rate, '__call__'):
+            lr *= self.decreasing_rate(self.iteration)
+        return lr
+
+    def _stop(self, dz):
+        '''Implement stopping criterion
+        '''
+
+        if self.iteration >= self.i_max or self.t >= self.t_max:
+            self.finished = True
+            return True
+
+        if self.stop == 'none':
+            return False
+
+        # If |x_n-x_n-1| < tol, stop
+        if dz < self.tol:
+            self.finished = True
+            log.info('Stop - No advance X - {}'.format(self.__repr__()))
+            return self.finished
+
+        # Other stopping criterion
+        if self.stop == 'up5':
+            return False
+        else:
+            return False
+
+    def start(self):
+        log.info('Start', self)
+        self.t0 = time()
+        self._init_algo()
+        if self.logging:
+            log.log_obj(name='cost'+str(self.id), obj=np.copy(self.pb.pt),
+                        iteration=self.iteration+1, fun=self.pb.cost,
+                        graph_cost=self.graph_cost, time=0)
+        self.t_start = time()
+
+    def end(self):
+        if self.logging:
+            log.log_obj(name='cost'+str(self.id), obj=self.pb.pt,
+                        iteration=self.iteration+1, fun=self.pb.cost,
+                        graph_cost=self.graph_cost, time=self.t)
+        log.info('End for {}'.format(self),
+                 'iteration {}, time {:.4}s'.format(self.iteration, self.t))
+        log.info('Total time: {:.4}s'.format(time()-self.t0))
